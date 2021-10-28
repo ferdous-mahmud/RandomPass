8ea04bf29da1cc13c2dbcd642b16ee363ab31aea
Ferdous <ferdous.webdev@gmail.com>
2021-10-28 11:54:50 +0600

    Update mainc.c
    new comment added

--- RandomPass/Model/mainc.c
+++ RandomPass/Model/mainc.c
@@ -5,12 +5,10 @@
 //  Created by Ferdous Mahmud Akash on 10/15/21.
 //
 #include "mainc.h"
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-#define MIN_CHAR 1
 #define BUF_SIZE 1024
 
 // Function to randomly generates password
@@ -20,7 +18,9 @@ const char* randomPasswordGeneration(int N)
     
     for (int i = 0; i < N; i++)
         password [i] = 33 + rand () % 94;
-
+    
+    
+//  indicate the termination of a character string
     password [N] = '\0';
     char *temp = password;
     return  temp;
